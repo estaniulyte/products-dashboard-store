4433a3c24e67ac3a0b25d79b515a834460febe28
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-17 16:53:41 +0300

    change: add children slot to InfoBox

--- components/InfoBox.vue
+++ components/InfoBox.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="grey lighten-5 bg:red red rounded text-center py-5">
+  <div class="grey lighten-5 bg:red red rounded text-center py-5 px-1">
     <p class="text-body-1 mb-2">
       <slot name="header"></slot>
     </p>
     <p class="ma-0 text-body-2">
       <slot name="description"></slot>
     </p>
+    <slot></slot>
   </div>
 </template>
 
