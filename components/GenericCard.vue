13c782f700b063889d57dfcf9f72f83670b0b3df
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-17 16:23:44 +0300

    feat: create Generic Card component

---
+++ components/GenericCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title class="d-flex justify-space-between">
+      <slot name="title"></slot>
+      <slot name="top-actions"></slot>
+    </v-card-title>
+    <v-card-subtitle>
+      <slot name="subtitle"></slot>
+    </v-card-subtitle>
+    <v-card-text>
+      <slot name="description"></slot>
+    </v-card-text>
+    <v-card-actions>
+      <slot name="actions"></slot>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "GenericCard",
+});
+</script>
