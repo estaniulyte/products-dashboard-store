06ade11f68b43d47a6ca25306ff348e7407baa9b
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-17 12:55:45 +0300

    feat: implement SearchBox for products

---
+++ components/SearchBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-text-field
+    v-model="query"
+    @input="handleInput"
+    prepend-inner-icon="mdi-magnify"
+    label="Search..."
+    hide-details
+    single-line
+    justify-sm
+    clearable
+    solo
+  />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "SearchBox",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    handleInput() {
+      this.$emit("search", this.query);
+    },
+  },
+});
+</script>
--- pages/Products.vue
+++ pages/Products.vue
@@ -1,17 +1,19 @@
 <template>
   <v-container>
+    <SearchBox class="my-4" @search="handleSearch" />
     <v-row>
       <v-col
         cols="12"
         sm="6"
         md="4"
-        v-for="product in products"
+        v-for="product in paginatedProducts"
         :key="product.id"
       >
         <ProductItem :product="product" @addToCart="addProductToCart" />
       </v-col>
     </v-row>
     <v-pagination
+      v-if="filteredProducts.length"
       v-model="currentPage"
       :length="totalPages"
       @input="changePage"
@@ -20,43 +22,68 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import ProductItem from '~/pages/ProductItem.vue';
+import { defineComponent } from "vue";
+import ProductItem from "~/pages/ProductItem.vue";
+import SearchBox from "../components/SearchBox.vue";
 
 export default defineComponent({
-  name: 'Products',
+  name: "Products",
   components: {
     ProductItem,
+    SearchBox,
+  },
+  data() {
+    return {
+      searchQuery: "",
+    };
   },
   computed: {
     products() {
-      const pageSize = 3;
-      const currentPage = this.$store.state.currentPage;
-      const products = this.$store.state.products;
-
-      const startIndex = (currentPage - 1) * pageSize;
-      const endIndex = startIndex + pageSize;
-
-      return products.slice(startIndex, endIndex);
+      return this.$store.state.products;
     },
     currentPage: {
       get() {
         return this.$store.state.currentPage;
       },
       set(value) {
-        this.$store.commit('SET_CURRENT_PAGE', value);
+        this.$store.commit("SET_CURRENT_PAGE", value);
       },
     },
+    pageSize() {
+      return 3;
+    },
     totalPages() {
-      return Math.ceil(this.$store.state.products.length / 3);
+      return Math.ceil(this.filteredProducts.length / this.pageSize);
+    },
+    filteredProducts() {
+      let tempProducts = [...this.products];
+      if (this.searchQuery != "" && this.searchQuery) {
+        return tempProducts.filter((product) => {
+          const query = this.searchQuery.toLowerCase();
+          return (
+            product.title.toLowerCase().includes(query) ||
+            product.description.toLowerCase().includes(query)
+          );
+        });
+      }
+      return tempProducts;
+    },
+    paginatedProducts() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.filteredProducts.slice(startIndex, endIndex);
     },
   },
   methods: {
     addProductToCart(product) {
-      this.$store.dispatch('addProductToCart', product);
+      this.$store.dispatch("addProductToCart", product);
     },
     changePage(value) {
       this.currentPage = value;
+    },
+    handleSearch(query) {
+      this.searchQuery = query;
+      this.currentPage = 1;
     },
   },
 });
