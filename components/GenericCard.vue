37f1ed39fb3454a6dde06aa74209dcbfeda070d4
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-18 14:52:46 +0300

    change: improve responsiveness of GenericCard

--- components/GenericCard.vue
+++ components/GenericCard.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-card>
-    <v-card-title class="d-flex justify-space-between">
-      <slot name="title"></slot>
-      <slot name="top-actions"></slot>
+  <v-card height="100%" class="d-flex flex-column">
+    <v-card-title class="d-flex justify-space-between text-h6">
+      <v-row>
+        <v-col cols="9" max-width="90%" class="text-no-wrap overflow-hidden">
+          <slot name="title"></slot>
+        </v-col>
+        <v-col cols="3" class="d-flex justify-end">
+          <slot name="top-actions"></slot>
+        </v-col>
+      </v-row>
     </v-card-title>
-    <v-card-subtitle>
+    <v-card-subtitle class="mt-1">
       <slot name="subtitle"></slot>
     </v-card-subtitle>
-    <v-card-text>
+    <v-card-text class="overflow-hidden text-wrap hide-text-overflow">
       <slot name="description"></slot>
     </v-card-text>
-    <v-card-actions>
+    <v-spacer></v-spacer>
+    <v-card-actions class="d-flex pa-3 align-end my-1" width="100%">
       <slot name="actions"></slot>
     </v-card-actions>
   </v-card>
@@ -23,3 +30,14 @@ export default defineComponent({
   name: "GenericCard",
 });
 </script>
+
+<style>
+.hide-text-overflow {
+  overflow: hidden;
+  max-height: 45px;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  white-space: nowrap;
+}
+</style>
