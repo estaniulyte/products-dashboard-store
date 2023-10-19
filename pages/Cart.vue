37c08997b985b5b4c12753e4a296f75a319f8503
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-19 19:00:14 +0300

    change: improve UX of Cart page

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -1,46 +1,58 @@
 <template>
   <v-container>
     <h1 class="mb-4">{{ $t("heading.Cart") }}</h1>
-    <v-simple-table v-if="cartItems.length">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th>{{ $t("table.Title") }}</th>
-            <th>{{ $t("table.Price") }}</th>
-            <th>{{ $t("table.Actions") }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in cartItems" :key="item.id">
-            <td>{{ $t("product.title." + item.title) }}</td>
-            <td>{{ $formatCurrency(item.price) }}</td>
-            <td>
-              <v-btn icon @click="incrementProductQuantity(item)">
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-              <v-btn icon @click="decrementProductQuantity(item)">
-                <v-icon>mdi-minus</v-icon>
-              </v-btn>
-              <v-btn icon @click="$emit('removeFromCart', item.id)">
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-            </td>
-          </tr>
-          <tr>
-            <td>{{ $t("table.Total") }}:</td>
-            <td>{{ $formatCurrency(total) }}</td>
-            <td>
-              <v-btn @click="checkout" color="primary">{{
-                $t("cart.Checkout")
-              }}</v-btn>
-              <v-btn @click="clearCart" text color="red">{{
-                $t("cart.Clear Cart")
-              }}</v-btn>
-            </td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <div v-if="cartItems.length">
+      <v-simple-table class="elevation-1">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>{{ $t("table.Title") }}</th>
+              <th>{{ $t("table.Price") }}</th>
+              <th class="pl-9">{{ $t("table.Amount") }}</th>
+              <th>{{ $t("table.Actions") }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in cartItems" :key="item.id">
+              <td>{{ $t("product.title." + item.title) }}</td>
+              <td>{{ $formatCurrency(item.price) }}</td>
+              <td>
+                <div class="">
+                  <v-btn icon @click="decrementProductQuantity(item)">
+                    <v-icon small>mdi-minus</v-icon>
+                  </v-btn>
+
+                  {{ item.quantity }}
+                  <v-btn icon @click="incrementProductQuantity(item)">
+                    <v-icon small>mdi-plus</v-icon>
+                  </v-btn>
+                </div>
+              </td>
+              <td>
+                <v-btn icon @click="removeProductFromCart(item)">
+                  <v-icon color="red">mdi-delete</v-icon>
+                </v-btn>
+              </td>
+            </tr>
+            <tr>
+              <td class="font-weight-bold">{{ $t("table.Total") }}:</td>
+              <td class="font-weight-bold">{{ $formatCurrency(total) }}</td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <br />
+      <div class="d-flex flex-row-reverse">
+        <v-btn @click="checkout" color="primary py-5 px-9 ml-5">{{
+          $t("cart.Checkout")
+        }}</v-btn>
+        <v-btn @click="clearCart" text color="red">{{
+          $t("cart.Clear Cart")
+        }}</v-btn>
+      </div>
+    </div>
     <InfoBox v-else>
       <template v-slot:header>{{ $t("cart.No items in cart") }}. </template>
       <template v-slot:description>
