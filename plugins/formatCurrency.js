45598ad47b28682ad616674d41c14945e3f14ca3
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-10-22 17:56:08 +0300

    change: add more validation to formatCurrency function

--- plugins/formatCurrency.js
+++ plugins/formatCurrency.js
@@ -1,7 +1,19 @@
-function formatCurrency(value) {
-  if (value == 0) return `$0`;
-  else if (value) return `$${value.toFixed(2)}`;
-  return;
+export function formatCurrency(value) {
+  if (typeof value === "string") {
+    const numericValue = parseFloat(value);
+    if (!isNaN(numericValue)) {
+      return `$${numericValue.toFixed(2)}`;
+    }
+  }
+
+  if (typeof value === "number" && !isNaN(value)) {
+    if (value == 0) return `$0`;
+    else if (value) return `$${value.toFixed(2)}`;
+  }
+
+  if (typeof value === "undefined") return "Empty value";
+
+  return "Invalid input. Pass nuber.";
 }
 
 export default (context, inject) => {
